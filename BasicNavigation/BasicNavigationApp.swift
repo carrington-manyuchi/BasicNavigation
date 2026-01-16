b4ea8387cd52f325faa4be78e33a38d2715304ae
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2026-01-16 17:59:47 +0200

    Initial Commit

---
+++ BasicNavigation/BasicNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicNavigationApp.swift
+//  BasicNavigation
+//
+//  Created by Manyuchi, Carrington C on 2026/01/16.
+//
+
+import SwiftUI
+
+@main
+struct BasicNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
